10f1587e1295b784b5e1a8f206a36733dbaa2bfb
Monowara Moni <monowaramonielination@gmail.com>
2024-09-27 15:49:18 +0600

    blog btn update

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 // blog btm link 
 
-// document.getElementById('blog-btn').addEventListener('click', function () {
-//      window.location.href = "./blog.html ";
- 
- 
-//  })
-//  console.log('h');
  document.getElementById('blog-btn').addEventListener ( 'click',function () {
-
      window.location.href = "./blog.html";
-     
  })
